d673246fb54f128e4cd8b1bdba74e0b1d3e2609d
Artur Zahorski <artur.zahorski@am.edu.pl>
2019-03-18 18:47:44 +0100

    Add Navbar component and navbar styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar';
 import './css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>APP</h1>
+        <Navbar />
       </div>
     );
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import '../css/Navbar.css';
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <nav className="nav-bar">
+        <h2 className="nav-logo">Oscars 2019</h2>
+        <ul className="nav-menu">
+          <li><a className="nav-menu-btn active" href="#home">Home</a></li>
+          <li><a className="nav-menu-btn" href="#actors">Best Actors</a></li>
+          <li><a className="nav-menu-btn" href="#actress">Best Actress</a></li>
+          <li><a className="nav-menu-btn" href="#films">Best Films</a></li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
